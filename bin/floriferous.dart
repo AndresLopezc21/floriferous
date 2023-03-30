218750fcdb95429020edb6510db8b3c9c792fc6d
AndresLopezc21 <andreszclyt@gmail.com>
2023-03-30 00:12:28 -0700

    deck shuffle test passed

--- bin/floriferous.dart
+++ bin/floriferous.dart
@@ -1,5 +1,36 @@
+import 'dart:math';
+
 import 'package:floriferous/floriferous.dart' as floriferous;
 
 void main(List<String> arguments) {
   print('Hello world: ${floriferous.calculate()}!');
+}
+
+class Card {
+  String name;
+  String color;
+  String size;
+  String shape;
+
+  Card(this.name, this.color, this.size, this.shape);
+}
+
+class Floriferous {
+  late List<Card> manoDeJugador;
+  late List<Card> deck;
+  Floriferous() {
+    deck = [
+      Card('Daisy', 'Yellow', 'Small', 'Round'),
+      Card('Rose', 'Red', 'Medium', 'Pointed'),
+      Card('Tulip', 'Pink', 'Large', 'Pointed'),
+      Card('Lily', 'White', 'Medium', 'Bell'),
+      Card('Pansy', 'Purple', 'Small', 'Round'),
+      Card('Orchid', 'Purple', 'Medium', 'Complex'),
+      Card('Sunflower', 'Yellow', 'Large', 'Round'),
+      Card('Hydrangea', 'Blue', 'Large', 'Cluster'),
+      Card('Carnation', 'Pink', 'Medium', 'Round'),
+      Card('Poppy', 'Red', 'Small', 'Complex'),
+    ];
+    deck.shuffle(Random());
+  }
 }
--- test/floriferous_test.dart
+++ test/floriferous_test.dart
@@ -1,8 +1,20 @@
+import 'dart:math';
+
 import 'package:floriferous/floriferous.dart';
 import 'package:test/test.dart';
+
+import '../bin/floriferous.dart';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
+  });
+
+  test('revolviendo el deck', () {
+    var juego = Floriferous();
+    var ordenOriginal = juego.deck.map((card) => card.name).toList();
+    juego.deck.shuffle();
+    var nuevoOrden = juego.deck.map((card) => card.name).toList();
+    expect(nuevoOrden, isNot(ordenOriginal));
   });
 }
